7ef708e0b7ac0019281b49cd4754603281baa5cf
Attakorah <99411652+Attakorah@users.noreply.github.com>
2026-06-18 21:48:08 +0000

    more learning

---
+++ JS Intro Course/the DOM/script.js
@@ -0,0 +1,26 @@
+//var a = document.getElementById('pOne');
+
+//var b = document.getElementsByTagName('p');
+
+// var c = document.getElementsByClassName('pCool');
+
+// var result = document.querySelector('.subheading');
+
+//var result = document.querySelectorAll("[title = 'more info']");
+
+//console.log(result);
+
+//var result1 = document.getElementsByClassName('subheading');
+
+//var result2 = document.querySelectorAll('.subheading');
+
+//console.log(result2.item(1));
+
+//for(let item of result2){
+//    console.log(item);
+//}
+
+var paraOne = document.querySelector('p');
+var paraTwo = document.getElementById('second');
+
+console.log(paraTwo.className);
